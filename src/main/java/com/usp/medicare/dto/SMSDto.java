7c6e155a0179386ecf40bc7c7447535bbcb04cdb
avishnoi05 <avishnoi05@yahoo.co.in>
2022-01-14 23:54:28 +0530

    add dto

---
+++ src/main/java/com/usp/medicare/dto/SMSDto.java
@@ -0,0 +1,22 @@
+package com.usp.medicare.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * SMSDto class for maintaining the SMS OTP details
+ * @author anuj
+ *
+ */
+@NoArgsConstructor @Getter @Setter 
+public class SMSDto {
+	
+	private String mobile;
+	private String source;
+	private String otp;
+	private String errorCode;
+	private String message;
+	private String languageCode;
+
+}
